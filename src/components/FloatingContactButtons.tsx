bf48b1bf542619ff232aef86090b57f045c03399
Acaj0 <antoniocarlosjr007@hotmail.com>
2024-11-01 17:24:02 -0400

    change blobs to bar

--- src/components/FloatingContactButtons.tsx
+++ src/components/FloatingContactButtons.tsx
@@ -1,44 +1,43 @@
 'use client'
 import { Button } from "@/components/ui/button"
 
-export default function FloatingContactButtons({ phoneNumber = "+12392860447" }) {
+export default function ContactBar({ phoneNumber = "+1239286-0447" }) {
   return (
-    <div className="fixed bottom-4 left-4 flex flex-col gap-2 z-50 md:hidden">
+    <div className="fixed bottom-0 left-0 right-0 bg-[#004a82] outline outline-white p-2 flex justify-between items-center z-50">
       <Button
-        variant="default"
-        size="icon"
-        className="rounded-full bg-green-500 hover:bg-green-600 transition-colors duration-200"
+        variant="ghost"
+        className="flex-1 text-white hover:bg-gray-700 transition-colors duration-200"
         onClick={() => window.open(`tel:${phoneNumber}`, '_self')}
-        aria-label="Call us"
       >
         <svg 
           xmlns="http://www.w3.org/2000/svg" 
           viewBox="0 0 512 512" 
-          className="h-5 w-5 text-white"
+          className="h-5 w-5 mr-2"
         >
           <path 
             fill="currentColor"
             d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z"
           />
         </svg>
+        Call
       </Button>
+      <div className="w-px h-8 bg-white mx-2"></div>
       <Button
-        variant="default"
-        size="icon"
-        className="rounded-full bg-blue-500 hover:bg-blue-600 transition-colors duration-200"
+        variant="ghost"
+        className="flex-1 text-white hover:bg-gray-700 transition-colors duration-200"
         onClick={() => window.open(`sms:${phoneNumber}`, '_self')}
-        aria-label="Send us an SMS"
       >
         <svg 
           xmlns="http://www.w3.org/2000/svg" 
           viewBox="0 0 512 512" 
-          className="h-5 w-5 text-white"
+          className="h-5 w-5 mr-2"
         >
           <path 
             fill="currentColor"
             d="M256 448c141.4 0 256-93.1 256-208S397.4 32 256 32S0 125.1 0 240c0 45.1 17.7 86.8 47.7 120.9c-1.9 24.5-11.4 46.3-21.4 62.9c-5.5 9.2-11.1 16.6-15.2 21.6c-2.1 2.5-3.7 4.4-4.9 5.7c-.6 .6-1 1.1-1.3 1.4l-.3 .3c0 0 0 0 0 0c0 0 0 0 0 0s0 0 0 0s0 0 0 0c-4.6 4.6-5.9 11.4-3.4 17.4c2.5 6 8.3 9.9 14.8 9.9c28.7 0 57.6-8.9 81.6-19.3c22.9-10 42.4-21.9 54.3-30.6c31.8 11.5 67 17.9 104.1 17.9zM96 212.8c0-20.3 16.5-36.8 36.8-36.8l19.2 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-19.2 0c-2.7 0-4.8 2.2-4.8 4.8c0 1.6 .8 3.1 2.2 4l29.4 19.6c10.3 6.8 16.4 18.3 16.4 30.7c0 20.3-16.5 36.8-36.8 36.8L112 304c-8.8 0-16-7.2-16-16s7.2-16 16-16l27.2 0c2.7 0 4.8-2.2 4.8-4.8c0-1.6-.8-3.1-2.2-4l-29.4-19.6C102.2 236.7 96 225.2 96 212.8zM372.8 176l19.2 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-19.2 0c-2.7 0-4.8 2.2-4.8 4.8c0 1.6 .8 3.1 2.2 4l29.4 19.6c10.2 6.8 16.4 18.3 16.4 30.7c0 20.3-16.5 36.8-36.8 36.8L352 304c-8.8 0-16-7.2-16-16s7.2-16 16-16l27.2 0c2.7 0 4.8-2.2 4.8-4.8c0-1.6-.8-3.1-2.2-4l-29.4-19.6c-10.2-6.8-16.4-18.3-16.4-30.7c0-20.3 16.5-36.8 36.8-36.8zm-152 6.4L256 229.3l35.2-46.9c4.1-5.5 11.3-7.8 17.9-5.6s10.9 8.3 10.9 15.2l0 96c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-48-19.2 25.6c-3 4-7.8 6.4-12.8 6.4s-9.8-2.4-12.8-6.4L224 240l0 48c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-96c0-6.9 4.4-13 10.9-15.2s13.7 .1 17.9 5.6z"
           />
         </svg>
+        SMS
       </Button>
     </div>
   )
